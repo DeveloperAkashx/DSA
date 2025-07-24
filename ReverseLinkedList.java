1907e4d82cce545b3074342683f5de3bfe85c893
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-07-24 07:13:03 -0700

    Reverse Linked List

---
+++ ReverseLinkedList.java
@@ -0,0 +1,46 @@
+public class ReverseLinkedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+        System.out.print("Original list: ");
+        printList(head);
+
+        ReverseLinkedList sol = new ReverseLinkedList();
+        ListNode reversedHead = sol.reverseList(head);
+
+        System.out.print("Reversed list: ");
+        printList(reversedHead);
+    }
+}
